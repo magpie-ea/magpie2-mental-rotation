85b7b068bf0d58880f4b10e97a214cea51f95def
marcelklehr <mklehr@gmx.net>
2023-05-12 18:53:01 +0000

    Deploying to gh-pages from @ magpie-ea/magpie3-mental-rotation@f1ba2dab1b2cb0f970992caaba7e196588f811cb 🚀

--- src/magpie.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  experimentId: '171',
-  serverUrl: 'https://magpie-demo.herokuapp.com/',
-  // this will be used in prolific mode
-  completionUrl: 'https://...',
-  contactEmail: 'test@random.com',
-  // Either 'debug', 'directLink' or 'prolific'
-  mode: 'debug'
-};
